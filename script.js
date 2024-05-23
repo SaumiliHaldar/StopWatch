95cd2204ee508e37ee7ab6f4ee4f7cb5689e1a7c
Saumili <haldar.saumili843@gmail.com>
2024-05-23 17:00:04 +0530

    added all the functions

--- script.js
+++ script.js
@@ -13,20 +13,32 @@ const startTimer = () => {
 // Function to stop the timer
 const stopTimer = () => {
     clearInterval (intervalId);
-}
+};
 
 // Function to reset the timer
 const resetTimer = () => {
     startCount = 0;
     countDownElement.innerText = startCount;
     clearInterval (intervalId);
-}
+};
+
+// Funtion to show the stop value
+const showStopValue = () => {
+    const newPara = document.createElement("p");
+    newPara.innerText = `The stop time is ${startCount-1}`;
+    resetValueElement.append(newPara);
+};
+
+// Funtion to show the stop time
+const clearTimerValue = () => {
+    resetValueElement.innerHTML = "";
+};
 
 
 // Event listeners
 document.querySelector(".startBtn").addEventListener("click", startTimer);
 
-document.querySelector(".stopBtn")..addEventListener("click", stopTimer);
+document.querySelector(".stopBtn").addEventListener("click", stopTimer);
 
 document.querySelector(".getTimeBtn").addEventListener("click", showStopValue);
 
